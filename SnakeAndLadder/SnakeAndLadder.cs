0704e315cf9848c8dc864253930177ce2b965d04
Aftab-1998 <alam41976@gmail.com>
2021-12-06 00:03:52 +0530

    [Aftab2] Add ; Addeed die Rolled

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -9,11 +9,30 @@ namespace SnakeAndLadder
     class SnakeAndLadder
     {
         private int playerPosition;
+        private Random objRandom;
+        private int DieRolled;
+        public SnakeAndLadder()
+        {
+            objRandom = new Random();
+        }
+
         public void StartGame()
         {
             Console.WriteLine("Game Started");
             Console.WriteLine("Player Position {0}", playerPosition);
+            RollADie();
         }
+        private void RollADie()
+        {
+            DieRolled = objRandom.Next(1, 7);
+            playerPosition = playerPosition + DieRolled;
+            int PlayerPosition = 0;
+            Console.WriteLine("Player Position: {0}", PlayerPosition);
+
+        }
+
+        
+
     }
 }
        
