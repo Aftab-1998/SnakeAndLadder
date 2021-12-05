c9d76177e93df475f025f035091258eb706e875a
Aftab-1998 <alam41976@gmail.com>
2021-12-05 23:48:21 +0530

    [Aftab1] Add ; Addeed Player at zzero position

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========Snake Ladder =======");
+            SnakeAndLadder objsnakeLadder = new SnakeAndLadder();
+            objsnakeLadder.StartGame();
+        }
+
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+        private int playerPosition;
+        public void StartGame()
+        {
+            Console.WriteLine("Game Started");
+            Console.WriteLine("Player Position {0}", playerPosition);
+        }
+    }
+}
+       
